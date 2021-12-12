3c82a00032a257a97a1a6ddf084b27fa836aadad
zoharerel <zoharerel@gmail.com>
2021-12-12 17:07:56 +0200

    console warnings fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     loginUrl: "http://localhost:3001/api/auth/login",
     registerUrl: "http://localhost:3001/api/auth/register",
     productsUrl: "http://localhost:3001/api/products/",
-    productImagesUrl: "https://localhost:3001/api/products/images/",
+    productImagesUrl: "http://localhost:3001/api/products/images/",
     ordersUrl: "http://localhost:3001/api/orders/",
     cartsUrl: "http://localhost:3001/api/carts/",
     cartItemsUrl: "http://localhost:3001/api/carts/cart-items/"
