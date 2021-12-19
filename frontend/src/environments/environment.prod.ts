57bf62c618530280490e4e713a573cdb9a53aec7
zoharerel <zoharerel@gmail.com>
2021-12-19 17:51:49 +0200

    test

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
 
-    loginUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/auth/login",
+    loginUrl: "https://ez-shop-beta.herokuapp.com/api/auth/login",
     registerUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/auth/register",
     productsUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/products/",
     productImagesUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/products/images/",
