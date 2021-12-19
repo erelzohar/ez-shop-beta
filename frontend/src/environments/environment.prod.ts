38b17de16dd2be5b81ca31417ad479d2d3eccb54
zoharerel <zoharerel@gmail.com>
2021-12-19 15:43:58 +0200

    api changed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
 
-    loginUrl: "https://shopping-site-beta.herokuapp.com/api/auth/login",
-    registerUrl: "https://shopping-site-beta.herokuapp.com/api/auth/register",
-    productsUrl: "https://shopping-site-beta.herokuapp.com/api/products/",
-    productImagesUrl: "https://shopping-site-beta.herokuapp.com/api/products/images/",
-    ordersUrl: "https://shopping-site-beta.herokuapp.com/api/orders/",
-    cartsUrl: "https://shopping-site-beta.herokuapp.com/api/carts/",
-    cartItemsUrl: "https://shopping-site-beta.herokuapp.com/api/carts/cart-items/"
+    loginUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/auth/login",
+    registerUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/auth/register",
+    productsUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/products/",
+    productImagesUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/products/images/",
+    ordersUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/orders/",
+    cartsUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/carts/",
+    cartItemsUrl: "https://cors-proxy-s.herokuapp.com/https://ez-shop-beta.herokuapp.com/api/carts/cart-items/"
 };
